a5546ca702bfa636d8e6cd40d2f6b0b945f462a3
Kristin Riebe <kriebe@aip.de>
2017-04-19 17:24:01 +0200

    Fixed classes for TAP_SCHEMA in core/models.py, fixture for main TAP_SCHEMA metadata added

--- core/models.py
+++ core/models.py
@@ -8,7 +8,7 @@ from django.utils.encoding import python_2_unicode_compatible
 # for the IVOA TAP specification
 @python_2_unicode_compatible
 class TAP_SCHEMA_schemas(models.Model):
-    schema_name = models.CharField(primary_key=True, max_length=256)
+    schema_name = models.CharField(max_length=256)
     utype = models.CharField(max_length=256, blank=True, null=True)
     description = models.CharField(max_length=1024, blank=True, null=True)
     
@@ -18,7 +18,7 @@ class TAP_SCHEMA_schemas(models.Model):
 
 @python_2_unicode_compatible
 class TAP_SCHEMA_tables(models.Model):
-    schema_name = models.CharField(primary_key=True, max_length=256)
+    schema_name = models.CharField(max_length=256)
     table_name = models.CharField(max_length=256)
     table_type = models.CharField(max_length=256)  # ???
     utype = models.CharField(max_length=256, blank=True, null=True)
@@ -26,17 +26,17 @@ class TAP_SCHEMA_tables(models.Model):
     table_index = models.IntegerField(null=True)  # used for sorting tables, 0 = first table, 1 = second table etc.
     
     def __str__(self):
-        return self.schema_name
+        return self.table_name
 
 
 @python_2_unicode_compatible
 class TAP_SCHEMA_columns(models.Model):
-    table_name = models.CharField(primary_key=True, max_length=256)
+    table_name = models.CharField(max_length=256)
     column_name = models.CharField(max_length=256)
     datatype = models.CharField(max_length=256)
-    arraysize = models.CharField(max_length=256, blank=True, null=True)
-    size = models.CharField(max_length=1024, blank=True, null=True)
-    description = models.IntegerField(null=True)  # used for sorting tables, 0 = first table, 1 = second table etc.
+    arraysize = models.IntegerField(null=True)
+    size = models.IntegerField(null=True)
+    description = models.CharField(max_length=1024, blank=True, null=True)
     utype = models.CharField(max_length=256, blank=True, null=True)
     unit = models.CharField(max_length=256, blank=True, null=True)
     ucd = models.CharField(max_length=256, blank=True, null=True)
@@ -46,13 +46,13 @@ class TAP_SCHEMA_columns(models.Model):
     column_index = models.NullBooleanField(default=False, null=True)
 
     def __str__(self):
-        return self.table_name
+        return self.column_name
 
 
 @python_2_unicode_compatible
 class TAP_SCHEMA_keys(models.Model):
     # describes foreign key relations between tables
-    key_id = models.CharField(primary_key=True, max_length=256)
+    key_id = models.CharField(max_length=256)
     from_table = models.CharField(max_length=256)
     target_table = models.CharField(max_length=256)
     description = models.CharField(max_length=256, blank=True, null=True)
@@ -63,9 +63,9 @@ class TAP_SCHEMA_keys(models.Model):
 
 
 @python_2_unicode_compatible
-class TAP_SCHEMA_keys(models.Model):
+class TAP_SCHEMA_key_columns(models.Model):
     # which columns can be used to join the tables; there may be more than one way to join tables
-    key_id = models.CharField(primary_key=True, max_length=256)
+    key_id = models.CharField(max_length=256)
     from_column = models.CharField(max_length=256)
     target_column = models.CharField(max_length=256)
 
