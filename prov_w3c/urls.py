d43709e9b4ae5a628570b9f1464212bebf9980a4
Kristin Riebe <kriebe@aip.de>
2017-05-05 22:28:43 +0200

    Fix url for w3c form

--- prov_w3c/urls.py
+++ prov_w3c/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     # activities
     url(r'^activities/$', views.ActivitiesView.as_view(), name='activities'),
     url(r'^activities/(?P<pk>[0-9a-zA-Z.:_-]+)/$', views.ActivityDetailView.as_view(), name='activity_detail'),
-    
+
     # entities
     url(r'^entities/$', views.EntitiesView.as_view(), name='entities'),
     url(r'^entities/(?P<pk>[0-9a-zA-Z.:_-]+)/$', views.EntityDetailView.as_view(), name='entity_detail'),
@@ -19,11 +19,11 @@ urlpatterns = [
     # agents
     url(r'^agents/$', views.AgentsView.as_view(), name='agents'),
     url(r'^agents/(?P<pk>[0-9a-zA-Z.:_-]+)/$', views.AgentDetailView.as_view(), name='agent_detail'),
-    
+
     # graphs
     url(r'^graph/$', views.graph, name='graph'),
     url(r'^graph/graphjson$', views.fullgraphjson, name='graphjson'),
-    
+
 #    url(r'^graph/mdpl2$', views.graphsingle, name='graph'),
 #    url(r'^graph/(?[0-9a-zA-Z.:_-]+)/graphjson$', views.graphjsonact, name='graphjsonact'),
 
